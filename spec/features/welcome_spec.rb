3aefbaf3119a9275af1d5ec98f4abb3edc1d0afd
diogeneshas <diogenes.henrique.br@gmail.com>
2021-12-13 14:15:07 -0300

    test: Verificar o link do cadastro de clientes

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 RSpec.feature "Welcomes", type: :feature do
   scenario "Mostra a mensagem de Bem-Vindo" do
-    visit('/')
+    visit(root_path)
     expect(page).to have_content('Bem-Vindo')
+  end
+
+  scenario "Verificar o link Cadastro de Clientes" do
+    visit(root_path)
+    # expect(page).to have_selector('ul li a')
+    # expect(page).to have_link('Cadastro de Clientes')
+    expect(find('ul li')).to have_link('Cadastro de Clientes')
   end
 end
